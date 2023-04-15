bc18b596a32b31b627c9620ff74d2a98511da462
jayanth-yadav <normal.mymail@gmail.com>
2023-04-15 17:21:14 +0530

    New commit

--- home.py
+++ home.py
@@ -2,4 +2,4 @@ import streamlit as st
 import pandas as pd
 st.header("Hellow")
 slider_value = st.slider(label="Slider",max_value=100,min_value=0,step=10)
-st.write(f"The slider value is {slider_value}")
+st.write(f"The slider value is {slider_value}.")
